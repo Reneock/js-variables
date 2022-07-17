8093a201cc750132e9d09489408956a7722d5c5a
Irene Ockrah <ostrene33@gmail.com>
2022-07-17 03:24:21 -0700

    javascript variables

---
+++ myscript.js
@@ -0,0 +1,28 @@
+	console.log(5 + 2);
+
+	var sex = girls + boys
+	console.log(sex)
+
+
+	var temperature = fahrenheit - celsius
+	console.log(temperature)
+
+	var volume = height * width * depth
+	console.log(volume)
+
+	var difference = aged / children
+	console.log(difference)
+
+	console.log (25 % 3);
+
+	console.log (++ absent);
+
+	console.log (-- absent);
+
+
+	let goodteams = ["liverpool", "Man Utd", "Barcelona", "Man City", "Arsenal"]
+
+	goodteams[4] = "Chelsea "
+console.log (goodteams)
+
+	
